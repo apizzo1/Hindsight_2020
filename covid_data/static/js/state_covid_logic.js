588e288d88a605838066f2f06010da32c2d8fd30
Maria Dong <maria.j.dong@gmail.com>
2020-09-22 13:58:30 -0400

    created new state covid fxn to take 'state' as an argument

--- covid_data/static/js/state_covid_logic.js
+++ covid_data/static/js/state_covid_logic.js
@@ -1,33 +1,107 @@
 // fxn for finding avg
-function find_avg (array) {
+function find_avg(array) {
     var sum = 0;
     for (var x = 0; x < array.length; x++) {
         sum += array[x];
     }
 
-    var avg = Math.round (sum / array.length);
+    var avg = Math.round(sum / array.length);
     return avg;
 }
 
 // create list of 50 states + DC
 var state_abbrs = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"];
 
+// create fxn that draws one sparkline, takes state as input
+function single_line_fxn(date, state) {
+
+    // format date; end result should be yyyymmdd for API calls
+    var moment_date = moment(date, 'YYYY-MM-DD');
+    var api_date = moment_date.format('YYYYMMDD');
+    // var prior_date = luxon_date.plus({ days: -1 }).toFormat('yyyyLLdd');
+
+    var state_url = `https://api.covidtracking.com/v1/states/${state}/daily.json`;
+
+    var case_array = [];
+    var date_array = [];
+    var death_array = [];
+
+    d3.json(state_url).then((response) => {
+        for (var x = 0; x < response.length; x++) {
+            date_array.push(response[x]['date']);
+            case_array.push(response[x]['positive']);
+            death_array.push(response[x]['death']);
+
+            if (response[x]['date'] == api_date) {
+                var select_cases = response[x]['positive'];
+                var select_deaths = response[x]['death'];
+                var select_index = x;
+            }
+        }
+
+        var case_increases = [];
+        for (var x = (case_array.length - 2); x > -1; x--) {
+            var increase = case_array[x] - case_array[x + 1];
+            case_increases.push(increase);
+
+            if (x == select_index) {
+                var select_increase = increase;
+                break;
+            }
+        }
+        
+        console.log(`total cases today: ${select_cases}, total deaths today: ${select_deaths}, new cases today: ${select_increase}`);
+
+        // find 7-day moving avgs for a smoother sparkline
+        var new_cases_avg = [];
+        for (var x = 0; x < case_increases.length; x++) {
+            var avg_array = [];
+            if (x > (case_increases.length - 8)) {
+                for (var y = x; y < (case_increases.length); y++) {
+                    avg_array.push(case_increases[y]);
+                }
+            }
+            else {
+                for (var y = 0; y < 7; y++) {
+                    avg_array.push(case_increases[x + y]);
+                }
+            }
+            var avg = find_avg(avg_array);
+            new_cases_avg.push(avg);
+        }
+
+        d3.select('#state_cases').append('span').classed(`${state}spark`, true);
+
+        // fxn for sparkline
+        $(function () {
+            $(`.${state}spark`).sparkline(new_cases_avg, {
+                width: '100px',
+                height: '75px',
+                minSpotColor: false,
+                maxSpotColor: false,
+                highlightSpotColor: 'red',
+                highlightLineColor: 'red'
+            });
+        });
+    });
+}
+
 // create fxn that draws sparklines; https://omnipotent.net/jquery.sparkline/#s-docs
 function state_lines_fxn(date) {
 
     // format date; end result should be yyyymmdd for API calls
     var moment_date = moment(date, 'YYYY-MM-DD');
-    var api_date = moment_date.format ('YYYYMMDD');
+    var api_date = moment_date.format('YYYYMMDD');
     // var prior_date = luxon_date.plus({ days: -1 }).toFormat('yyyyLLdd');
 
     var state_data = state_data_fxn();
 
-    state_abbrs.forEach ((state) => {
-        state_data[state].then ((response) => {
-            
+    state_abbrs.forEach((state) => {
+        state_data[state].then((response) => {
+
             var case_array = [];
             for (var x = (response.length - 1); x > 0; x--) {
-                case_array.push (response[x]['cases'])
+                case_array.push(response[x]['cases'])
                 if (response[x]['date'] == api_date) {
                     break
                 }
@@ -36,7 +110,7 @@ function state_lines_fxn(date) {
             var case_increases = [];
             for (var x = (case_array.length - 1); x > 0; x--) {
                 var increase = case_array[x] - case_array[x - 1];
-                case_increases.push (increase);
+                case_increases.push(increase);
             }
 
             // find 7-day moving avgs for a smoother sparkline
@@ -45,22 +119,22 @@ function state_lines_fxn(date) {
                 var avg_array = [];
                 if (x > (case_increases.length - 8)) {
                     for (var y = x; y < (case_increases.length); y++) {
-                        avg_array.push (case_increases[y]);
+                        avg_array.push(case_increases[y]);
                     }
                 }
                 else {
                     for (var y = 0; y < 7; y++) {
-                        avg_array.push (case_increases[x + y]);
+                        avg_array.push(case_increases[x + y]);
                     }
                 }
-                var avg = find_avg (avg_array);
-                new_cases_avg.push (avg);
+                var avg = find_avg(avg_array);
+                new_cases_avg.push(avg);
             }
 
-            d3.select ('#state_cases').append ('span').classed (`${state}spark`, true);
+            d3.select('#state_cases').append('span').classed(`${state}spark`, true);
 
             // fxn for sparkline
-            $(function() {
+            $(function () {
                 $(`.${state}spark`).sparkline(new_cases_avg.reverse(), {
                     width: '70px',
                     height: '40px',
@@ -96,8 +170,8 @@ async function getStateData(state) {
 
     try {
         const response = await d3.json(state_url);
-        response.forEach ((data) => {
-            state_data.push ({
+        response.forEach((data) => {
+            state_data.push({
                 date: data.date,
                 cases: data.positive,
                 deaths: data.death
@@ -111,4 +185,5 @@ async function getStateData(state) {
     }
 }
 
-state_lines_fxn('2020-08-15');
+// state_lines_fxn('2020-08-15');
+single_line_fxn('2020-07-14', 'GA');
