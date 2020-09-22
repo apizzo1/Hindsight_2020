2fe1e09614d82752fe88e14e47878b2418041d1d
Ryan Jones <ryanjones@Ryans-MacBook-Air.local>
2020-09-22 13:56:46 -0400

    added tables and resources

--- Ryan/SQL_Queries/table_schemata.sql
+++ Ryan/SQL_Queries/table_schemata.sql
@@ -17,3 +17,34 @@ create table "national_mobility" (
 	"gps_residential" float NOT NULL,
 	"gps_away_from_home" float NOT NULL
 );
+
+create table "state_ids" (
+	id int NOT NULL,
+	state varchar NOT NULL,
+	state_abbrev varchar NOT NULL,
+	population_2019 int NOT NULL,
+	PRIMARY KEY (id)
+);
+
+create table "state_mobility"(
+	"year" int NOT NULL,
+	"month" int NOT NULL,
+	"day" int NOT NULL,
+	id int NOT NULL,
+	"gps_retail_and_recreation" float NOT NULL,
+	"gps_grocery_and_pharmacy" float NOT NULL,
+	"gps_parks" float NOT NULL,
+	"gps_transit_stations" float NOT NULL,
+	"gps_workplaces" float NOT NULL,
+	"gps_residential" float NOT NULL,
+	"gps_away_from_home" float NOT NULL
+);
+
+create table "state_cases"(
+	"year" int,
+	"month" int,
+	"day" int,
+	id int not null,
+	"case_count" int,
+	"new_case_count" int
+);
