1b74a31171b5bfc737743c530b651362d45a3bd8
Ryan Jones <ryanjones@Ryans-MacBook-Air.local>
2020-09-23 13:39:54 -0400

    comparison plots

--- Ryan/templates/polar.js
+++ Ryan/templates/polar.js
@@ -46,6 +46,18 @@ d3.csv('../Resources/Google Mobility - National - Daily.csv').then(function (inp
     type: "scatterpolar",
     mode: "lines",
     name: "Retail",
+    r: [-2, datasets[0].retail, datasets[0].retail, -2],
+    theta: [0, 0, 72, 0],
+    fill: "toself",
+    fillcolor: '#E4FF87',
+    line: {
+      color: 'black'
+    }
+  }
+  var daytrace1 = {
+    type: "scatterpolar",
+    mode: "lines",
+    name: "Retail",
     r: [-2, datasets[180].retail, datasets[180].retail, -2],
     theta: [0, 0, 72, 0],
     fill: "toself",
@@ -55,6 +67,18 @@ d3.csv('../Resources/Google Mobility - National - Daily.csv').then(function (inp
     }
   }
   var trace2 = {
+    type: "scatterpolar",
+    mode: "lines",
+    name:"Parks",
+    r: [-2, datasets[0].parks, datasets[0].parks, -2],
+    theta: [0, 72, 144, 0],
+    fill: "toself",
+    fillcolor: 'red',
+    line: {
+      color: 'black'
+    }
+  }
+  var daytrace2 = {
     type: "scatterpolar",
     mode: "lines",
     name:"Parks",
@@ -70,6 +94,18 @@ d3.csv('../Resources/Google Mobility - National - Daily.csv').then(function (inp
     type: "scatterpolar",
     mode: "lines",
     name:"Grocery",
+    r: [-2, datasets[0].grocery, datasets[0].grocery, -2],
+    theta: [0, 144, 216, 0],
+    fill: "toself",
+    fillcolor: 'blue',
+    line: {
+      color: 'black'
+    }
+  }
+  var daytrace3 = {
+    type: "scatterpolar",
+    mode: "lines",
+    name:"Grocery",
     r: [-2, datasets[180].grocery, datasets[180].grocery, -2],
     theta: [0, 144, 216, 0],
     fill: "toself",
@@ -79,6 +115,18 @@ d3.csv('../Resources/Google Mobility - National - Daily.csv').then(function (inp
     }
   }
   var trace4 = {
+    type: "scatterpolar",
+    mode: "lines",
+    name:"Transit",
+    r: [-2, datasets[0].transit, datasets[0].transit, -2],
+    theta: [0, 216, 288, 0],
+    fill: "toself",
+    fillcolor: 'orange',
+    line: {
+      color: 'black'
+    }
+  }
+  var daytrace4 = {
     type: "scatterpolar",
     mode: "lines",
     name:"Transit",
@@ -94,6 +142,18 @@ d3.csv('../Resources/Google Mobility - National - Daily.csv').then(function (inp
     type: "scatterpolar",
     mode: "lines",
     name:"Office",
+    r: [-2, datasets[0].office, datasets[0].office, -2],
+    theta: [0, 288, 360, 0],
+    fill: "toself",
+    fillcolor: 'purple',
+    line: {
+      color: 'black'
+    }
+  }
+  var daytrace5 = {
+    type: "scatterpolar",
+    mode: "lines",
+    name:"Office",
     r: [-2, datasets[180].office, datasets[180].office, -2],
     theta: [0, 288, 360, 0],
     fill: "toself",
@@ -103,8 +163,10 @@ d3.csv('../Resources/Google Mobility - National - Daily.csv').then(function (inp
     }
   }
   var data =[trace1, trace2, trace3, trace4, trace5]
+  var data2 =[daytrace1, daytrace2, daytrace3, daytrace4, daytrace5]
+
   var layout = {
-    title:"What Are You Up To?",
+    title:"Baseline 2020",
     polar: {
       radialaxis: {
         visible: true,
@@ -113,6 +175,18 @@ d3.csv('../Resources/Google Mobility - National - Daily.csv').then(function (inp
     },
     showlegend: true
   }
+  var layout2 = {
+    title:`${datasets[180].date}`,
+    polar: {
+      radialaxis: {
+        visible: true,
+        range: [-2, 2]
+      }
+    },
+    showlegend: false
+  }
   console.log(datasets[180].retail, datasets[180].parks)
-  Plotly.newPlot('chart', data, layout);
+  Plotly.newPlot('chart', data2, layout2);
+  Plotly.newPlot('basechart', data, layout);
+
 })
