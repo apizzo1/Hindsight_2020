b05b173a4493960ab20715e026932889e5f2a344
Becki Oeswein Leeds <rmoesw01@gmail.com>
2020-09-23 20:01:42 -0400

    restructure folders to account for flask wrapper

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,1 @@
 finnhub_API_Key = "btla6h748v6omckuq520"
-Polygon_API_Key = "KXQPHVRu6ZsaxBwaKuKQQPy_O8zQQSGn"
--- static/js/stock.js
+++ static/js/stock.js
@@ -1,18 +1,63 @@
-// Polygon_API_Key = "KXQPHVRu6ZsaxBwaKuKQQPy_O8zQQSGn"
-// baseURL = 'https://api.polygon.io/v1/open-close/'
-// ticker = 'AMZN'
-// date = '2020-09-21'
+sliderDate = Math.floor(new Date('09-22-2020').getTime()/1000.0)
+baseUrl = 'https://finnhub.io/api/v1/';
+category = 'stock/candle';
+symbol = 'AMZN';
+startdate = new Date('December 31, 2019').getTime()/1000.0;
+// startdate = '1572651390'
+enddate = sliderDate;
+var humanEndDate = new Date( sliderDate *1000);
+// enddate = '1572910590'
+finnhub_API_Key = "btla6h748v6omckuq520"
 
-// queryURL = `${baseURL}${ticker}/${date}?apiKey=${Polygon_API_Key}`
+queryUrl = `${baseUrl}${category}?symbol=${symbol}&resolution=D&from=${startdate}&to=${enddate}&token=${finnhub_API_Key}`;
+console.log(queryUrl);
 
-// d3.json(queryURL).then(function(stockData) {
-//     console.log(stockData);
-// });
+d3.json(queryUrl).then(function(stockData) {
+  console.log(stockData);
+  var convDates = [];
+  for (var i=0; i<stockData.t.length; i++) {
+    convDates.push(new Date( stockData.t[i] *1000));
+  }
+  var stockTrace = {
+    x: convDates,
+    y: stockData.c,
+    // decreasing: {line: {color: '#7F7F7F'}}, 
+    // high: stockData.h,
+    // increasing: {line: {color: '#17BECF'}},
+    // line: {color: 'rgba(31,119,180,1)'},
+    // low: stockData.l,
+    // open: stockData.o,
+    type: 'scatter',
+    xaxis: 'x',
+    yaxis: 'y',
+  };
 
-const request = require('request');
+  var data = [stockTrace];
 
-request('https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=1&from=1572651390&to=1572910590&token=', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log(body.url);
-  console.log(body.explanation);
+  var layout = {
+    dragmode: 'zoom', 
+    margin: {
+      r: 10, 
+      t: 25, 
+      b: 40, 
+      l: 60
+    }, 
+    showlegend: false, 
+    xaxis: {
+      autorange: true, 
+      domain: [0, 10], 
+      range: ['12-31-2019', humanEndDate], 
+      rangeslider: {range: ['12-31-2019', humanEndDate]}, 
+      title: 'Date', 
+      type: 'date'
+    }, 
+    yaxis: {
+      autorange: true, 
+      domain: [0, 1], 
+      range: [100, 200], 
+      type: 'linear'
+    }
+  };
+  
+  Plotly.newPlot('stockChart', data, layout);
 });
