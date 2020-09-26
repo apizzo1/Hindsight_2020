8ccdd3e67bce69074e8d2e5288cbb4007b8123bd
Maria Dong <maria.j.dong@gmail.com>
2020-09-25 20:50:33 -0400

    added dict to accept full state names

--- master/static/js/GT-project/state_plot.js
+++ master/static/js/GT-project/state_plot.js
@@ -59,7 +59,92 @@ function stateUnemployment(state, date) {
         Plotly.newPlot('stateUnemp', plotData, layout);
     });   
 }
-function single_state_fxn(state, date) {
+
+function single_state_fxn(full_state, date) {
+    
+    // modified version of https://gist.github.com/calebgrove/c285a9510948b633aa47; returns abbreviation or full state name
+    function abbrRegion(input, to) {
+        var states = [
+            ['Alabama', 'AL'],
+            ['Alaska', 'AK'],
+            ['American Samoa', 'AS'],
+            ['Arizona', 'AZ'],
+            ['Arkansas', 'AR'],
+            ['Armed Forces Americas', 'AA'],
+            ['Armed Forces Europe', 'AE'],
+            ['Armed Forces Pacific', 'AP'],
+            ['California', 'CA'],
+            ['Colorado', 'CO'],
+            ['Connecticut', 'CT'],
+            ['Delaware', 'DE'],
+            ['District Of Columbia', 'DC'],
+            ['Florida', 'FL'],
+            ['Georgia', 'GA'],
+            ['Guam', 'GU'],
+            ['Hawaii', 'HI'],
+            ['Idaho', 'ID'],
+            ['Illinois', 'IL'],
+            ['Indiana', 'IN'],
+            ['Iowa', 'IA'],
+            ['Kansas', 'KS'],
+            ['Kentucky', 'KY'],
+            ['Louisiana', 'LA'],
+            ['Maine', 'ME'],
+            ['Marshall Islands', 'MH'],
+            ['Maryland', 'MD'],
+            ['Massachusetts', 'MA'],
+            ['Michigan', 'MI'],
+            ['Minnesota', 'MN'],
+            ['Mississippi', 'MS'],
+            ['Missouri', 'MO'],
+            ['Montana', 'MT'],
+            ['Nebraska', 'NE'],
+            ['Nevada', 'NV'],
+            ['New Hampshire', 'NH'],
+            ['New Jersey', 'NJ'],
+            ['New Mexico', 'NM'],
+            ['New York', 'NY'],
+            ['North Carolina', 'NC'],
+            ['North Dakota', 'ND'],
+            ['Northern Mariana Islands', 'NP'],
+            ['Ohio', 'OH'],
+            ['Oklahoma', 'OK'],
+            ['Oregon', 'OR'],
+            ['Pennsylvania', 'PA'],
+            ['Puerto Rico', 'PR'],
+            ['Rhode Island', 'RI'],
+            ['South Carolina', 'SC'],
+            ['South Dakota', 'SD'],
+            ['Tennessee', 'TN'],
+            ['Texas', 'TX'],
+            ['US Virgin Islands', 'VI'],
+            ['Utah', 'UT'],
+            ['Vermont', 'VT'],
+            ['Virginia', 'VA'],
+            ['Washington', 'WA'],
+            ['West Virginia', 'WV'],
+            ['Wisconsin', 'WI'],
+            ['Wyoming', 'WY'],
+        ];
+        
+        var i; // Reusable loop variable
+        if (to == 'abbr') {
+            input = input.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
+            for (i = 0; i < states.length; i++) {
+                if (states[i][0] == input) {
+                    return (states[i][1]);
+                }
+            }
+        } else if (to == 'name') {
+            input = input.toUpperCase();
+            for (i = 0; i < states.length; i++) {
+                if (states[i][1] == input) {
+                    return (states[i][0]);
+                }
+            }
+        }
+    }
+
     function find_avg (array) {
         var sum = 0;
         for (var x = 0; x < array.length; x++) {
@@ -69,8 +154,11 @@ function single_state_fxn(state, date) {
         var avg = Math.round (sum / array.length);
         return avg;
     }
+
+    var state = abbrRegion(full_state, 'abbr');
+
     // format date; end result should be yyyymmdd for API calls
-    var moment_date = moment.unix(date/1000).add(1, 'days');
+    var moment_date = moment.unix(date/1000);
     var api_date = moment_date.format('YYYYMMDD');
     // var prior_date = luxon_date.plus({ days: -1 }).toFormat('yyyyLLdd');
 
