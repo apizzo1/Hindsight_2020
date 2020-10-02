36306fc078d8c7ab0263d70cddf2287d0c5e3b6a
apizzo1 <63465031+apizzo1@users.noreply.github.com>
2020-10-01 22:54:10 -0400

    Update logic_slider_map.js

--- static/js/GT-project/logic_slider_map.js
+++ static/js/GT-project/logic_slider_map.js
@@ -1,4 +1,4 @@
-var API_KEY = process.env.API_KEY;
+// var API_KEY = process.env.API_KEY;
 // add light tile layer 
 var light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
